9c80719d8fff8c6e121d2c8655d6b46f8c907353
Roman Klepov <rklepov@gmail.com>
2021-12-27 23:00:21 +0300

    feat: implement reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
+// index.js
+
+function reverse2(a, r) {
+    if (!r) return a;
+    if (r < 0) return reverse2(a, -r);
+    const d = r % 10;
+    r /= 10;
+    r |= 0;
+    return reverse2(a * 10 + d, r);
 }
+
+function reverse(n) {
+    return reverse2(0, n);
+}
+
+module.exports = reverse;
+
+//__EOF__
